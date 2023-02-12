49c6f764728b94f41e3e170465a3896057621651
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-12 23:03:53 +0000

    Adedd a isBroken property to the scooter class constructor

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,5 @@
 class Scooter{
-  constructor(station, username){
+  constructor(station, username, isBroken = Boolean /* charge */ /* serial */ /* nextSerial */){
     if(!station){
       this.station = null
       this.username = username
@@ -8,7 +8,24 @@ class Scooter{
       this.station = station
       this.username = null
     }
+    
+    this.isBroken = isBroken
+    // this.charge = charge
+    // if(!serial){
+    //   this.serial += nextSerial
+    //   nextSerial += 1
+    // }
+
   }
+  // static nextSerial = 1
+  /* this.serial = serial */
+  /*serial is a static methoid that starts from 1
+  each time a new serial is assigned to a scooter incraese next serial by 1 
+  use the value of next serial to assign serial a value*/
+  
+  
 }
 
-module.exports = Scooter
+// let scooter1 = new Scooter("London", "Tobyha01", 50)
+// console.log(scooter1)
+module.exports = Scooter 
