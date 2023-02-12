8628c4330fc241b2d695498903de17a9c663b65d
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-12 15:26:39 +0000

    Created first scooter test

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,13 +1,21 @@
+/* station: string; the station the scooter is located at or null if checked out
+user: the User who checked out the Scooter, or null if docked
+serial: a number assigned sequentially from nextSerial
+nextSerial: a static number which starts at 1 and increments each time a new serial number is assigned
+charge: a number from 0 (no charge at all) to 100 (fully charged)
+isBroken: boolean */
+
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
+/* Scooter object checks */
+describe('scooter object checks', function() {
+  test('checks if scooter is an object', function() {
+    let scooter1 = new Scooter
+    expect(typeof scooter1).tobe("object");
+  });
+
+  
 })
 
 //Method tests
